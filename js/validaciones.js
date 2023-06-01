1a3e0335cb01cc8f77e49e9a9eff3b44f709be4c
Jarl Kallagher <z_amr145@hotmail.com>
2023-06-01 09:39:56 -0600

    Validacion de correo exitosa

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,31 +1,19 @@
-//validaciones de formularios
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const form = document.getElementById("form");
-const alerta = document.getElementById("alerta");
+//validar Correo Electronico
+const enviar = document.getElementById('enviar');
+const alerta = document.getElementById('alerta');
 
-form.addEventListener("submit", e => { 
+enviar.addEventListener('click', (e) => { 
     e.preventDefault();
-    let warnings = "";
-    let entrar = false;
-    let alerta = "";
+    const email = document.getElementById('email').value;
+    validarCorreo();
+})
+
+const validarCorreo = (correo) => { 
     let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    alerta.innerHTML = ""
-    if(nombre.value.length < 4) { 
-        warnings += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if(!mailFormat.test(email.value)) { 
-        warnings += `El email no es valido <br>`
-        entrar = true
-    }
-    if(message.test(message.value)) {
-        warnings += `El campo del mensaje esta vacio <br>`
-        entrar = true
-    }
-    if(entrar) { 
-        parrafo.innerHTML = warnings;
+    let verificar = mailFormat.test(correo);
+    if(verificar) { 
+        alerta.innerHTML = 'El correo es valido'
     } else {
-        parrafo.innerHTML = "Enviado";  
+        alerta.innerHTML = 'El correo NO es valido'
     }
-});
+}
